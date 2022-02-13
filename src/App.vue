58a5d7aa86b0b24c6557d6a28f18393face74e01
Chester <nestor_chavez@hotmail.es>
2022-02-12 22:26:54 -0600

    Añadiendo componente de conversor de monedas, usando API externa

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Moneda-Conversor/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MonedaConversor from './components/MonedaConversor.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MonedaConversor
   }
 }
 </script>
